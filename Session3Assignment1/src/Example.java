424ed5412cc7c87e9fa06409ed48276aa3c9965f
gouree <goure.deshpande@gmail.com>
2017-03-30 12:19:49 +0530

    Add files via upload

---
+++ Session3Assignment1/src/Example.java
@@ -0,0 +1,19 @@
+/*
+ * What will happen if you try to compile and run this?
+			 
+ */
+public class Example
+{
+			
+			static
+			{
+			print(10);
+			}
+			static void print (int x) 
+			{
+			System.out.println (x);
+			System.exit(0);
+			}
+}
+	
+
